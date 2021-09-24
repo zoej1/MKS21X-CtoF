7aeb8a3a63d769d86697aa6582d536d31caf6fe8
Zoe Jiang <zjiang30@stuy.edu>
2021-09-24 02:44:09 -0400

    first commit

---
+++ CtoFTester.java
@@ -0,0 +1,16 @@
+/* The function should have celsius and fahrenheit as parameters.
+They should be the type double. The function should return a double.*/
+public class CtoFTester{
+  public static double celsiusToFahrenheit(double celsius) {
+    return celsius * (9.0/5.0) + 32.0;
+  }
+
+  public static double fahrenheitToCelsius(double fahrenheit) {
+    return (fahrenheit - 32.0) * (5.0/9.0);
+  }
+
+  public static void main(String[] args) {
+    System.out.println(celsiusToFahrenheit(40.0));
+    System.out.println(fahrenheitToCelsius(100.0));
+  }
+}
